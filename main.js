9af2576110ca61b6593ab797201ae2cc7b5f0b93
Ig0rD <34953521+Ig0rD@users.noreply.github.com>
2019-10-23 15:42:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const addbtn = document.querySelector("input.add");
-const form = document.querySelector("form");
 const txt = document.querySelector("input.inptxt");
 const tbody = document.querySelector("tbody");
 
-localStorage.setItem("vetprodutos", array());
+if(localStorage.getItem("vetprodutos") === null){
+    localStorage.setItem("vetprodutos", "[]");
+} else {
+    localdata = JSON.parse(localStorage.getItem("vetprodutos"));
+    for(let i=0;i<localdata.length;i++){
+        list(localdata[i]);
+    }
+}
+
 addbtn.addEventListener('click', function (e) {
     e.preventDefault();
     let newdata = txt.value;
+    addprod(newdata);
     if (typeof (Storage) !== "undefined") {
 
     }
@@ -15,17 +23,29 @@ addbtn.addEventListener('click', function (e) {
 tbody.addEventListener('click', function (e) {
     e.preventDefault();
     if(event.target.type == 'submit') {
-        localStorage.removeItem(event.target.parentNode.parentNode.firstChild.firstChild.textContent);
+        remprod(event.target.parentNode.parentNode.firstChild.firstChild.textContent);
         event.target.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode);
     }
 });
 function list(a){
-    let prod = a.value
+    let prod;
+    if(typeof a == "string") {
+        prod = a;
+    } else {
+        prod = a.value;
+    }
     const btn = `<button>Excluir</button>`;
     let td = `<td>${prod}</td><td>${btn}</td>`;
     let tr = `<tr>${td}</tr>`;
     tbody.innerHTML += tr;
 }
 function addprod(prod){
-    
+    vetres = JSON.parse(localStorage.getItem("vetprodutos"));
+    vetres.push(prod);
+    localStorage.setItem("vetprodutos", JSON.stringify(vetres));
+}
+function remprod(prod){
+    vetres = JSON.parse(localStorage.getItem("vetprodutos"));
+    vetres.splice(vetres.indexOf(prod), 1)
+    localStorage.setItem("vetprodutos", JSON.stringify(vetres));
 }
